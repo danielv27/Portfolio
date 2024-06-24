359df705832e459cdb7824c16a2cf89ce997ab91
Daniel Verner <danielv2000@gmail.com>
2024-06-25 01:52:58 +0200

    tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,vue}",
+    ],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            'light-black': '#0B0C10',
+            'dark-blue': '#1F2833',
+            'gray': '#C5C6C7',
+            'light-blue': '#66FCF1',
+            'green': '#45A29E',
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
+    },
+    plugins: [],
+}
