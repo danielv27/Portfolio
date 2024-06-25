c5fbf604dc138b1c1b97e20bf5ad891778e91d42
Daniel Verner <danielv2000@gmail.com>
2024-06-25 14:51:03 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ function addSmoothScrolling() {
     link.addEventListener('click', function (e) {
       e.preventDefault();
 
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
+      const targetId = link?.getAttribute('href')?.substring(1);
+      const targetElement = document.getElementById(targetId ?? '');
 
       if (targetElement) {
         window.scrollTo({
