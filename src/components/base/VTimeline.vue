0eaaeae104b5fe9fff6a2d2c6166c48bf61c51df
Daniel Verner <danielv2000@gmail.com>
2024-06-28 14:25:47 +0200

    Better alignment of company logo in timeline

--- src/components/base/VTimeline.vue
+++ src/components/base/VTimeline.vue
@@ -1,95 +1,40 @@
 <template>
-  <div @mouseenter="setShow(true)" @mouseleave="setShow(false)" class="flex w-1/2 h-144">
-    <ul class=" w-1/12 timeline timeline-vertical text-white" >
-      <li :class="listClass" @mouseenter="setIndex(0)">
-        <div class="timeline-start" :class="{'text-dark-blue': currentIndex === 0}">Sep 2021</div>
+  <div @mouseenter="setShow(true)" class="flex w-1/2 h-144">
+    <ul class="w-1/12 timeline timeline-vertical text-white">
+      <li
+          v-for="(entry, index) in entries"
+          :key="index"
+          class="transition ease-in-out delay-75 bg-blue-500 active:text-dark-blue hover:scale-110 duration-300"
+          :class="{'text-dark-blue' : currentIndex === index}"
+          @mouseenter="setIndex(index)"
+      >
+        <hr v-if="index !== 0" />
+        <div class="timeline-start">{{ entry.date }}</div>
         <div class="timeline-middle">
           <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
               fill="currentColor"
-              class="h-5 w-5">
+              class="h-5 w-5"
+          >
             <path
                 fill-rule="evenodd"
                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"/>
+                clip-rule="evenodd"
+            />
           </svg>
         </div>
-        <hr/>
-      </li>
-      <li :class="listClass" @mouseenter="setIndex(1)">
-        <hr/>
-        <div class="timeline-start" :class="{'text-dark-blue': currentIndex === 1}">Feb 2022</div>
-        <div class="timeline-middle ">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5">
-            <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"/>
-          </svg>
-        </div>
-        <hr/>
-      </li>
-      <li :class="listClass" @mouseenter="setIndex(2)">
-        <hr/>
-        <div class=" timeline-start" :class="{'text-dark-blue': currentIndex === 2}">Nov 2022</div>
-        <div class="timeline-middle">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5">
-            <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"/>
-          </svg>
-        </div>
-        <hr/>
-      </li>
-      <li :class="listClass" @mouseenter="setIndex(3)">
-        <hr/>
-        <div class="timeline-start" :class="{'text-dark-blue': currentIndex === 3}">Nov 2022</div>
-        <div class="timeline-middle">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5">
-            <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"/>
-          </svg>
-        </div>
-        <hr/>
-      </li>
-      <li :class="listClass" @mouseenter="setIndex(4)">
-        <hr/>
-        <div class="timeline-start" :class="{'text-dark-blue': currentIndex === 4}">Aug 2023</div>
-        <div class="timeline-middle">
-          <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5">
-            <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"/>
-          </svg>
-        </div>
+        <hr v-if="index !== entries.length - 1" />
       </li>
     </ul>
     <Transition :duration="550" name="nested">
-      <div class="mt-5 p-5 w-11/12 h-[73%] bg-dark-blue rounded-b-4xl rounded-r-4xl" v-if="show">
-        <div class="inner">
+      <div v-if="show" class="mt-5 px-6 pt-5 w-11/12 h-[73%] bg-dark-blue rounded-b-4xl rounded-r-4xl">
+        <div class="inner h-full flex flex-col justify-between">
+          <div>
           <h1 class="text-2xl">{{ currentEntry.title }}</h1>
           <div>{{ currentEntry.content }}</div>
+          </div>
+          <img class="max-w-32 self-end" :src="currentEntry.icon" :alt="currentEntry.title">
         </div>
       </div>
     </Transition>
@@ -97,17 +42,17 @@
 </template>
 
 <script setup lang="ts">
-import vuSvg from '@assets/vu.svg'
-
-import {computed, ref} from "vue";
+import vuSvg from '@assets/vu.svg';
+import { computed, ref } from 'vue';
 
 const show = ref(false);
-const setShow = (value: boolean) => show.value = value;
+const setShow = (value: boolean) => (show.value = value);
 
-const currentIndex = ref(0);
-const setIndex = (index: number) => currentIndex.value = index;
+const currentIndex = ref<number>(0);
+const setIndex = (index: number) => (currentIndex.value = index);
 
 interface Entry {
+  date: string;
   title: string;
   content: string;
   icon: string;
@@ -115,48 +60,51 @@ interface Entry {
 
 const entries: Entry[] = [
   {
+    date: 'Sep 2021',
     title: 'Teaching Assistant, Vrije Universiteit Amsterdam',
-    content: '',
+    content: 'Work with smaller groups of Computer Science and AI students to reinforce the learning process for various courses they take along the year with the main focus on developing knowledge and coding skills.',
     icon: vuSvg
   },
   {
+    date: 'Feb 2022',
     title: 'Full Stack Developer, DBV Software Solutions',
     content: '',
     icon: vuSvg
   },
   {
+    date: 'Nov 2022',
     title: 'Frontend Lead, Zorgplein.online',
     content: '',
     icon: vuSvg
   },
   {
+    date: 'Nov 2022',
     title: 'Full Stack Developer, Capisoft B.V.',
     content: '',
     icon: vuSvg
   },
   {
+    date: 'Aug 2023',
     title: 'Software Engineer, DongIT',
     content: '',
     icon: vuSvg
   }
-]
+];
 
-const listClass = "transition ease-in-out delay-75 bg-blue-500 hover:text-dark-blue hover:scale-110 hover:bg-indigo-500 duration-300";
 
 const currentEntry = computed(() => entries[currentIndex.value]);
-
-
-
 </script>
-<style scoped>
 
+<style scoped>
 li {
   height: 90px;
 }
 
-.nested-enter-active, .nested-leave-active {
+.nested-enter-active,
+.nested-leave-active {
   transition: all 0.3s ease-in-out;
 }
+
 /* delay leave of parent element */
 .nested-leave-active {
   transition-delay: 0.25s;
@@ -173,6 +121,7 @@ li {
 .nested-leave-active .inner {
   transition: all 0.3s ease-in-out;
 }
+
 /* delay enter of nested element */
 .nested-enter-active .inner {
   transition-delay: 0.25s;
