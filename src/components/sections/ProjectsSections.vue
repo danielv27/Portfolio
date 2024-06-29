2fe8adaeb6ea7d4e90ba32b204d0d0aacac03d47
Daniel Verner <danielv2000@gmail.com>
2024-06-29 14:24:43 +0200

    added projects + build dist image

--- src/components/sections/ProjectsSections.vue
+++ src/components/sections/ProjectsSections.vue
@@ -2,55 +2,68 @@
   <div id="projects" class="projects-section pt-12 px-5">
     <h2 class="text-3xl font-bold mb-6">Projects</h2>
     <div class="projects-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      <div v-for="(project, index) in projects" :key="index" class="project-card bg-lighter-dark-blue rounded-lg shadow-md p-6">
-        <h3 class="text-xl font-semibold mb-2">{{ project.title }}</h3>
-        <p class="text-gray-700">{{ project.description }}</p>
-        <a :href="project.link" class="text-blue-500 hover:underline mt-4 inline-block">View Project</a>
+      <div v-for="(project, index) in projects" :key="index"
+           class="flex flex-col project-card bg-lighter-dark-blue rounded-lg shadow-md p-6 justify-between">
+        <div>
+          <h3 class="text-xl font-semibold mb-2">{{ project.title }}</h3>
+          <p class="text-gray-700">{{ project.description }}</p>
+        </div>
+        <div class="mt-8 flex justify-between">
+          <a :href="project.link" target="_blank" class="text-blue-500 hover:underline inline-block">View
+            Project</a>
+          <img v-if="project.image" class="max-w-8 rounded object-contain" :src="project.image" :alt="project.title"/>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import atLargeResearchImage from '@assets/atLargeReseach.png'
+import elasticLogo from '@assets/elastic.svg'
+
 const projects = [
   {
-    title: 'TypeScript EDSL',
-    description: 'For my BSc thesis I designed and implemented and embedded domain specific language ' +
-    'in Typescript. The aim of this EDSL was to streamline the configuration process and reduce errors for ' +
-    'projects in which the configuration values are predefined.',
-    image: 'path/to/project1.jpg',
-    link: 'https://link-to-project1.com'
+    title: 'TS EDSL',
+    description: 'As part of my BSc thesis, I designed and implemented and embedded domain specific language (EDSL) '
+        + 'in Typescript. The EDSL was created to streamline the configuration process through Typescript ' +
+        'type-safety features and VS-Code snippets',
+    image: atLargeResearchImage,
+    link: 'https://github.com/atlarge-research/continuum/tree/7ee6d908fafe9985337f42e104fec90de13357fe/input/dsl/src'
   },
   {
-    title: 'University Project 2',
-    description: 'Description of project 2.',
-    image: 'path/to/project2.jpg',
-    link: 'https://link-to-project2.com'
+    title: 'KB-Indexer',
+    description: 'As part of the course "DevOps and Cloud-based Software" I took in my masters, my peers and I migrated'
+        + '"KB-Indexer", a monolithic search engine used for research purposes, to a scalable cloud application deployed on Azure.' +
+        'To manage and monitor indexing jobs we used Kubernetes, CronJobs (Scheduler), Helm charts and Grafana. Additionally, we created an API  ' +
+        'and a web-app in Vue to make use of these features in an interactive web application.\n',
+    image: elasticLogo,
+    link: 'https://github.com/DevOps-Group20'
   },
-  {
-    title: 'University Project 3',
-    description: 'Description of project 3.',
-    image: 'path/to/project3.jpg',
-    link: 'https://link-to-project3.com'
-  },
-  {
-    title: 'University Project 4',
-    description: 'Description of project 1.',
-    image: 'path/to/project1.jpg',
-    link: 'https://link-to-project1.com'
-  },
-  {
-    title: 'University Project 5',
-    description: 'Description of project 2.',
-    image: 'path/to/project2.jpg',
-    link: 'https://link-to-project2.com'
-  },
-  {
-    title: 'University Project 6',
-    description: 'Description of project 3.',
-    image: 'path/to/project3.jpg',
-    link: 'https://link-to-project3.com'
-  }
+  // {
+  //   title: 'University Project 3',
+  //   description: 'Description of project 3.',
+  //   image: 'path/to/project3.jpg',
+  //   link: 'https://link-to-project3.com'
+  // },
+  // {
+  //   title: 'University Project 4',
+  //   description: 'Description of project 1.',
+  //   image: 'path/to/project1.jpg',
+  //   link: 'https://link-to-project1.com'
+  // },
+  // {
+  //   title: 'University Project 5',
+  //   description: 'Description of project 2.',
+  //   image: 'path/to/project2.jpg',
+  //   link: 'https://link-to-project2.com'
+  // },
+  // {
+  //   title: 'University Project 6',
+  //   description: 'Description of project 3.',
+  //   image: 'path/to/project3.jpg',
+  //   link: 'https://link-to-project3.com'
+  // }
 ];
 </script>
 
