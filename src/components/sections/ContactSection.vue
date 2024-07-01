f5cd19a0c44603879b812d9f1975868b2bc38841
Daniel Verner <danielv2000@gmail.com>
2024-07-01 02:55:00 +0200

    fixed contact section

--- src/components/sections/ContactSection.vue
+++ src/components/sections/ContactSection.vue
@@ -6,24 +6,24 @@
         <div class="mb-4">
           <label for="name" class="block text-sm font-medium">Name</label>
           <input type="text" id="name" v-model="form.name" placeholder="Enter your name"
-                 required class="mt-1 text-dark-blue block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm">
+                 required class="mt-1 text-dark-blue dark:text-white block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm">
         </div>
 
         <div class="mb-4">
           <label for="email" class="block text-sm font-medium">Email</label>
           <input type="email" id="email" v-model="form.email" placeholder="Enter your email"
-                 required class="mt-1 text-dark-blue block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm">
+                 required class="mt-1 text-dark-blue dark:text-white bg-dark block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm">
         </div>
         <div class="mb-4">
           <label for="subject" class="block text-sm font-medium">Subject</label>
           <input type="text" id="subject" v-model="form.subject" placeholder="Enter subject"
-                 required class="mt-1 text-dark-blue block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm">
+                 required class="mt-1 text-dark-blue dark:text-white block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm">
         </div>
 
         <div class="mb-4">
           <label for="message" class="block text-sm font-medium">Message</label>
           <textarea id="message" v-model="form.message" rows="4" placeholder="Enter your message"
-                    required class="mt-1 text-dark-blue block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm"></textarea>
+                    required class="mt-1 text-dark-blue dark:text-white block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-green sm:text-sm"></textarea>
         </div>
 
         <button type="submit" :disabled="loading" class="flex justify-center items-center w-20 h-10 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-lighter-dark-blue hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
